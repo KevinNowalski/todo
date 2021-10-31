970e342993600d36109972483b55269e40cdb344
Kevin Nowalski <kevinnowalskisite@gmail.com>
2021-10-31 02:58:54 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  list:any[]=[]
+  addTodo(item:string){
+    this.list.push({id:this.list.length,name:item})
+  }
+  removeTodo(id:number){
+    this.list=this.list.filter(item=>item.id!==id)
+  }
 }
