ed595cb0e47686a0bad0fdc7d798d3f26e2986db
Kevin Nowalski <kevinnowalskisite@gmail.com>
2021-11-01 17:15:31 -0400

    Styling and functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,22 +9,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   list:any[]=[]
 
+  todo: string = ''
+
+  toggle: any = {};
+
   todoFound: boolean = false
+
 
   addTodo(item:string){
 
     this.todoFound = this.list.some(el => el.name === item);
 
     if(this.todoFound === true){
-      alert('TODO ALREADY EXISTS!')
+      alert(item + ' already exists!')
     }else{
       this.todoFound = false
       this.list.push({id:this.list.length,name:item})
       console.log(this.list)
     }
   }
+
   removeTodo(id:number){
     this.list=this.list.filter(item=>item.id!==id)
+    if(this.toggle[id] === true){
+      this.toggle[id] = !this.toggle[id]
+    }
     console.log(this.list)
   }
+
 }
